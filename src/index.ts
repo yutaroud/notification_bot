2a51219e620030c0302fb00b077bf8453ca98f03
Watanabe Yuki <watanabe.yuki@a-tm.co.jp>
2020-04-26 14:26:45 +0900

    nullの場合にエラーが発生するため修正

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,18 @@ let notifier = new Notification();
 const envItemId = process.env.itemId ? process.env.itemId : "";
 const envTargetPrice = process.env.TARGET_PRICE ? process.env.TARGET_PRICE : 0;
 const convertInteger = (stringNum: string) => {
-  return stringNum.replace(/[^0-9]/g, "");
+  const replaceNum = stringNum.replace(/[^0-9]/g, "");
+  if (typeof replaceNum === "number") {
+    return replaceNum;
+  } else {
+    return 0;
+  }
 };
 
 const notifyTargetPrice = () => {
   observer.getAmazonPrice(envItemId).then((price: string) => {
-    if (convertInteger(price) < envTargetPrice) {
-      notifier.postNotification(convertInteger(price));
+    if (convertInteger(price) < envTargetPrice && convertInteger(price) !== 0) {
+      notifier.postNotification(price);
     } else {
       console.log("まだ目標金額より高いです。");
     }
